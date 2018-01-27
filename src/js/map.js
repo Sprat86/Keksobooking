ecb9a70c4067e4bca7120e21923c75bdbae2a83a
Sprat86 <spratlist@gmail.com>
2018-01-27 16:40:05 +0300

    Create DOM element btn_map

--- src/js/map.js
+++ src/js/map.js
@@ -1,4 +1,3 @@
-let map = [];
 const titles = [
     "Большая уютная квартира"
     , "Маленькая неуютная квартира"
@@ -21,11 +20,14 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-for (let i = 1; map.length < 8; i++) {
+let mapEl = document.getElementsByClassName('map');
+
+    for (let i = 1; i < 9; i++) {
     let x = '1',
         y = '2';
 
     const checkIn = checkin[getRandomInt(0, 2)];
+
     let myObj = {
         author: {avatar: "img/avatars/user0" + i},
         offer: {
@@ -37,7 +39,7 @@ for (let i = 1; map.length < 8; i++) {
             guests: getRandomInt(1, 10),
             checkin: checkIn,
             checkout: checkIn,
-            features: features.slice(Math.floor(Math.random() * ((features.length-1) + 1))),
+            features: features.slice(Math.floor(Math.random() * ((features.length - 1) + 1))),
             description: "",
             photos: []
         },
@@ -45,9 +47,22 @@ for (let i = 1; map.length < 8; i++) {
             x: getRandomInt(300, 900),
             y: getRandomInt(100, 500)
         }
-   };
-    map.push(myObj)
+    };
+    createElement(mapEl, myObj)
 }
 
 
+function createElement(mapEl, publication) {
+    let btn_map = document.createElement("btn");
+    btn_map.setAttribute('class', 'map__pin');
+    btn_map.style = "left: " + publication.location.x + "px; top:" + publication.location.y + "px;";
+
+    let avatarImg = document.createElement('img');
+    avatarImg.setAttribute('src', publication.author.avatar + '.png');
+    avatarImg.setAttribute("height", "40");
+    avatarImg.setAttribute("width", "40");
+    avatarImg.setAttribute("draggable", "false");
+    btn_map.appendChild(avatarImg);
+    mapEl[0].appendChild(btn_map);
+}
 
