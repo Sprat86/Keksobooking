5719c02a22d7a3297ee1d79411857374fb061c86
Sprat86 <spratlist@gmail.com>
2018-02-25 21:02:28 +0300

    changes in function mapPin[i].addEventListener (work to classes map__pin--active and 'hidden')

--- src/js/map.js
+++ src/js/map.js
@@ -1,4 +1,3 @@
-
 const titles = [
     "Большая уютная квартира"
     , "Маленькая неуютная квартира"
@@ -21,7 +20,7 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-let mapEl = document.getElementsByClassName('map');
+let mapEl = document.getElementsByClassName('map')[0];
 let mapPins = document.getElementsByClassName('map__pins');
 let RUBLE = '\u20bd';
 
@@ -52,19 +51,19 @@ for (let i = 1; i < 9; i++) {
         }
     };
 
-    createElement(mapEl, publication);
+    createElement(publication);
     createElementArticle(mapEl, publication);
 }
 
 // Create DOM element btn_map
 
-function createElement(mapEl, publication) {
+function createElement(publicationCreatingEl) { // Задаем функции параметр "publicationCreatingEl" и внутри функции работаем с этим параметром.
     let buttonMap = document.createElement("button");
     buttonMap.setAttribute('class', 'map__pin');
-    buttonMap.style = "left: " + publication.location.x + "px; top:" + publication.location.y + "px;";
+    buttonMap.style = "left: " + publicationCreatingEl.location.x + "px; top:" + publicationCreatingEl.location.y + "px;";
 
     let avatarImg = document.createElement('img');
-    avatarImg.setAttribute('src', publication.author.avatar + '.png');
+    avatarImg.setAttribute('src', publicationCreatingEl.author.avatar + '.png');
     avatarImg.setAttribute("height", "40");
     avatarImg.setAttribute("width", "40");
     avatarImg.setAttribute("draggable", "false");
@@ -74,12 +73,12 @@ function createElement(mapEl, publication) {
 
 // Create DOM element notice
 
-function createElementArticle(mapEl, publication) {
+function createElementArticle(mapEl, publicationCreatingElement) { // Задаем функции параметр "publicationCreatingElement" и внутри функции работаем с этим параметром.
     let publicationBlock = document.createElement("article");
     publicationBlock.setAttribute('class', 'map__card popup');
 
     let popupAvatar = document.createElement('img');
-    popupAvatar.setAttribute('src', publication.author.avatar + '.png');
+    popupAvatar.setAttribute('src', publicationCreatingElement.author.avatar + '.png');
     popupAvatar.setAttribute('class', 'popup__avatar');
     popupAvatar.setAttribute("height", "70");
     popupAvatar.setAttribute("width", "70");
@@ -93,56 +92,55 @@ function createElementArticle(mapEl, publication) {
     publicationBlock.appendChild(popupClose);
 
     let title = document.createElement('h3');
-    let h3 = document.createTextNode(publication.offer.title);
+    let h3 = document.createTextNode(publicationCreatingElement.offer.title);
     title.appendChild(h3);
     publicationBlock.appendChild(title);
 
     let address = document.createElement('p');
-    let pAddress = document.createTextNode(publication.offer.address);
+    let pAddress = document.createTextNode(publicationCreatingElement.offer.address);
     address.appendChild(pAddress);
     publicationBlock.appendChild(address);
 
     let price = document.createElement('p');
-    let pPrice = document.createTextNode(publication.offer.price + ' ' + RUBLE + '/ночь');
+    let pPrice = document.createTextNode(publicationCreatingElement.offer.price + ' ' + RUBLE + '/ночь');
     price.setAttribute('class', 'popup_price');
     price.appendChild(pPrice);
     publicationBlock.appendChild(price);
 
     let type = document.createElement('h4');
-    let h4 = document.createTextNode(getTypeTranslation(publication.offer.type));
+    let h4 = document.createTextNode(getTypeTranslation(publicationCreatingElement.offer.type));
     type.appendChild(h4);
     publicationBlock.appendChild(type);
 
     let roomsGuest = document.createElement('p');
-    let pRoomsGuest = document.createTextNode(publication.offer.rooms + ' комнаты для ' + publication.offer.guests + ' гостей');
+    let pRoomsGuest = document.createTextNode(publicationCreatingElement.offer.rooms + ' комнаты для ' + publicationCreatingElement.offer.guests + ' гостей');
     roomsGuest.appendChild(pRoomsGuest);
     publicationBlock.appendChild(roomsGuest);
 
     let checkInCheckOut = document.createElement('p');
-    let pCheckInCheckOut = document.createTextNode('Заезд после ' + publication.offer.checkin + ' , выезд после ' + publication.offer.checkout);
+    let pCheckInCheckOut = document.createTextNode('Заезд после ' + publicationCreatingElement.offer.checkin + ' , выезд после ' + publicationCreatingElement.offer.checkout);
     checkInCheckOut.appendChild(pCheckInCheckOut);
     publicationBlock.appendChild(checkInCheckOut);
 
 
     let features = document.createElement('ul');
     features.setAttribute('class', 'popup__features');
-    for(let i = 0; i < publication.offer.features.length; i++)
-    {
+    for (let i = 0; i < publicationCreatingElement.offer.features.length; i++) {
         let li = document.createElement('li');
-        li.setAttribute('class', 'feature feature--' + publication.offer.features[i]);
+        li.setAttribute('class', 'feature feature--' + publicationCreatingElement.offer.features[i]);
         features.appendChild(li);
         publicationBlock.appendChild(features)
     }
 
 
     let description = document.createElement('p');
-    let pDescription = document.createTextNode(publication.offer.description);
+    let pDescription = document.createTextNode(publicationCreatingElement.offer.description);
     description.appendChild(pDescription);
     publicationBlock.appendChild(description);
 
 
-    mapEl[0].appendChild(publicationBlock);
-    console.log(publication.offer.type)
+    mapEl.appendChild(publicationBlock);
+    console.log(publicationCreatingElement.offer.type)
 }
 
 
@@ -156,114 +154,115 @@ function getTypeTranslation(type) {
 }
 
 
-
-
 // Add style for "window.onload"
 
-let mapId = document.getElementById ("map");
+let mapId = document.getElementById("map");
 let noticeId = document.getElementById("notice");
 let fieldset = document.getElementsByTagName("fieldset");
 let mapPin = document.querySelectorAll('.map__pin');
 let mapCard = document.querySelectorAll('.map__card');
 
-window.onload = function() {
+window.onload = function () {
     mapId.classList.add('map--faded');
     noticeId.classList.add("notice__form--disabled");
-    for( let i = 0; i < fieldset.length; i++ ) {
+    for (let i = 0; i < fieldset.length; i++) {
         fieldset[i].setAttribute("disabled", "disabled");
-        }
-        mapPinHide (mapPin);
-        mapCardHide(mapCard);
-        mapPinMain.style = "display: block";
+    }
+    mapPinHide(mapPin);
+    mapCardHide(mapCard);
+    mapPinMain.style = "display: block";
 };
 
 
-function mapPinHide (mapPin){
-    for( let i = 0; i < mapPin.length; i++ ) {
-          // mapPin[i].style = "display: none"; - Перезапишет существующее значение style (в данном случае координаты) - использовать нельзя.
-             mapPin[i].style.display = "none"; // Добавит стиль к существующему
-               }
-       }
-
-
-function mapCardHide (mapCard){
-    for( let i = 0; i < mapCard.length; i++ ) {
-        mapCard[i].classList.add('hidden');
-           }
+function mapPinHide(mapPin) {
+    for (let i = 0; i < mapPin.length; i++) {
+        // mapPin[i].style = "display: none"; - Перезапишет существующее значение style (в данном случае координаты) - использовать нельзя.
+        mapPin[i].style.display = "none"; // Добавит стиль к существующему
     }
+}
 
+
+function mapCardHide(mapCard) {
+    for (let i = 0; i < mapCard.length; i++) {
+        mapCard[i].classList.add('hidden');
+    }
+}
 
 
 // Remove slyle for "mouseup"
 
 let mapPinMain = document.querySelector('.map__pin--main');
 mapPinMain.addEventListener("mouseup", function () {
-    mapId.classList.remove('map--faded');
-    noticeId.classList.remove("notice__form--disabled");
-    for( let i = 0; i < fieldset.length; i++ ) {
-        fieldset[i].disabled = false;
+        mapId.classList.remove('map--faded');
+        noticeId.classList.remove("notice__form--disabled");
+        for (let i = 0; i < fieldset.length; i++) {
+            fieldset[i].disabled = false;
         }
-        mapPinShow (mapPin);
-       }
+        mapPinShow(mapPin);
+    }
 );
 
-function mapPinShow (mapPin){
-    for( let i = 0; i < mapPin.length; i++ ) {
+function mapPinShow(mapPin) {
+    for (let i = 0; i < mapPin.length; i++) {
         //mapPin[i].style = "display: block";
         mapPin[i].style.display = ""; // убирает стиль display
     }
 }
 
 
-function mapCardShow (mapCard){
-    for( let i = 0; i < mapCard.length; i++ ) {
+function mapCardShow(mapCard) {
+    for (let i = 0; i < mapCard.length; i++) {
         mapCard[i].classList.remove('hidden');
     }
 };
 
 
-
-
 // Show/hide mapCard (notice):
 let ESC_KEYCODE = 27;
 let ENTER_KEYCODE = 13;
+let mapPinActive = document.getElementsByClassName('map__pin--active'); // Берём элемент с классом 'map__pin--active'. Так как он всегда на странице один,
+// то и работает с ним, как с массивом с одним элементом.
+
 for (let i = 1; i < mapPin.length; i++) {
-    mapPin[i].addEventListener("click", function () {
-            // mapPin[i].classList.toggle('map__pin--active'); //- добавляет/убирает класс при повторном клике на элементе.
-            if (mapPin[i].classList.contains('map__pin--active')) {
-                mapPin[i].classList.remove('map__pin--active');
-                mapCard[i - 1].classList.add('hidden');
-            } else {
-                mapPin[i].classList.add('map__pin--active');
-                mapCard[i - 1].classList.remove('hidden');
+    mapPin[i].addEventListener("click", function (event) {
+
+        if (mapPinActive.length) {
+            mapPinActive[0].classList.remove('map__pin--active');
+        }
+        mapCardHide(mapCard); // Вызываем функцию, чтобы каждый раз присваивать класс 'hidden' карточкам объявления. Тогда при удалении класса (см. ниже) показывается только одна карточка.
+
+        //mapPin[i].classList.toggle('map__pin--active'); //- добавляет/убирает класс при повторном клике на элементе.
+
+        if (!mapPin[i].classList.contains('map__pin--active')) {
+            mapPin[i].classList.add('map__pin--active');
+            mapCard[i - 1].classList.remove('hidden');
             }
-            document.addEventListener('keydown', function(evt) {
-                if (evt.keyCode === ESC_KEYCODE || evt.keyCode === ENTER_KEYCODE) {
-                    mapPin[i].classList.remove('map__pin--active');
-                    mapCard[i - 1].classList.add('hidden');
-                }
-            });
-        popupClose[i-1].focus();
-            });
-    }
+        document.addEventListener('keydown', function (evt) {
+            if (evt.keyCode === ESC_KEYCODE || evt.keyCode === ENTER_KEYCODE) {
+                mapPin[i].classList.remove('map__pin--active');
+                mapCard[i-1].classList.add('hidden');
+            }
+        });
+        popupClose[i - 1].focus();
+    });
+}
 
 
 
-    //
-    // $(".map__pin").click(function(e) {
-    //     e.preventDefault();
-    //     $(".map__pin").removeClass('map__pin--active');
-    //     $(this).addClass('map__pin--active');
-    // });
-
+// Рабочий код JQuery:
+// $(".map__pin").click(function(e) {
+//     e.preventDefault();
+//     $(".map__pin").removeClass('map__pin--active');
+//     $(this).addClass('map__pin--active');
+// });
 
 
 /** Действия на кнопку "Крестик" в объявлении.*/
 let popupClose = document.querySelectorAll('.popup__close');
-for( let i = 0; i < popupClose.length; i++ ) {
-popupClose[i].addEventListener("click", function () {
+for (let i = 0; i < popupClose.length; i++) {
+    popupClose[i].addEventListener("click", function () {
         mapCard[i].classList.add('hidden');
-        mapPin[i+1].classList.remove('map__pin--active');
+        mapPin[i + 1].classList.remove('map__pin--active');
     });
     popupClose[i].addEventListener("keydown", function (evt) {
         if (evt.keyCode === ENTER_KEYCODE) {
@@ -271,9 +270,7 @@ popupClose[i].addEventListener("click", function () {
             mapPin[i + 1].classList.remove('map__pin--active');
         }
     });
-    }
-
-
+}
 
 
 /** Работа с формой объявления:*/
@@ -293,36 +290,34 @@ let guestsForm = document.querySelector('#capacity');
 
 
 /** Проверка правильности введенных данных полей "Заголовок" и "Адрес":*/
-titleForm.addEventListener('invalid', function(evt){
-      if(titleForm.validity.tooShort){
+titleForm.addEventListener('invalid', function (evt) {
+    if (titleForm.validity.tooShort) {
         titleForm.setCustomValidity('Пожалуйста, введите сообщение длиной не менее 30 символов.')
-    /*} else if (titleForm.validity.tooLong){
-        titleForm.setCustomValidity('Пожалуйста, введите сообщеине длиной не более 100 символов')*/
-    } else if (titleForm.validity.valueMissing){
+        /*} else if (titleForm.validity.tooLong){
+            titleForm.setCustomValidity('Пожалуйста, введите сообщеине длиной не более 100 символов')*/
+    } else if (titleForm.validity.valueMissing) {
         titleForm.setCustomValidity('Введите, пожалуйста, заголовок объявления!')
     } else {
-          titleForm.setCustomValidity('');
-      }
+        titleForm.setCustomValidity('');
+    }
 });
 
 
-addressForm.addEventListener('invalid', function(evt){
-   if (addressForm.validity.valueMissing) {
-       addressForm.setCustomValidity('Без этого поля мы не сможем Вас найти')
-   }
-    });
-
+addressForm.addEventListener('invalid', function (evt) {
+    if (addressForm.validity.valueMissing) {
+        addressForm.setCustomValidity('Без этого поля мы не сможем Вас найти')
+    }
+});
 
 
 /** Поля «время заезда» и «время выезда» - синхронизация:*/
-timeInForm.addEventListener('change', function(){
+timeInForm.addEventListener('change', function () {
     timeOutForm.value = timeInForm.value;
-    });
+});
 
-timeOutForm.addEventListener('change', function(){
+timeOutForm.addEventListener('change', function () {
     timeInForm.value = timeOutForm.value;
-  });
-
+});
 
 
 /** Значение поля «Тип жилья» синхронизировано с минимальной ценой:*/
@@ -333,7 +328,7 @@ let minPrice = {
     palace: 10000
 };
 
-flatTypeForm.addEventListener('change', function(){
+flatTypeForm.addEventListener('change', function () {
     switch (flatTypeForm.options.selectedIndex) {
         case 0:
             priceForNightForm.setAttribute('min', minPrice.flat);
@@ -355,9 +350,8 @@ flatTypeForm.addEventListener('change', function(){
 });
 
 
-
 /** Количество комнат связано с количеством гостей: */
-roomsForm.addEventListener('change', function(){
+roomsForm.addEventListener('change', function () {
     switch (roomsForm.options.selectedIndex) {
         case 0:
             guestsForm.options[2].selected = true;                       // Устанавливает вариант списка по умолчанию.
@@ -392,17 +386,17 @@ roomsForm.addEventListener('change', function(){
 
 
 /** Проверка валидности формы:*/
-for (let i = 0; i <inputsForm.length; i++) {
+for (let i = 0; i < inputsForm.length; i++) {
     inputsForm[i].addEventListener('invalid', function (evt) {
-       if (inputsForm[i].validity.valueMissing) {
+        if (inputsForm[i].validity.valueMissing) {
             inputsForm[i].style.border = "2px solid red";
-           inputsForm[i].style.background = 'rgba(222, 213, 213, .5)';
-           inputsForm[i].setAttribute('placeholder', 'Заполните это поле.')
-          } else if (inputsForm[i].validity.valid) {
-           inputsForm[i].style.border = "";
-           }
-           });
-   }
+            inputsForm[i].style.background = 'rgba(222, 213, 213, .5)';
+            inputsForm[i].setAttribute('placeholder', 'Заполните это поле.')
+        } else if (inputsForm[i].validity.valid) {
+            inputsForm[i].style.border = "";
+        }
+    });
+}
 
 
 
