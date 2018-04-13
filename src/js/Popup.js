c16e3ef2f086e2ecb42abd3312453b44564a1ea8
Sprat86 <spratlist@gmail.com>
2018-04-13 19:03:24 +0300

    Editing file backend.js. Optimization code. Create module popup.js for receive a server response

---
+++ src/js/Popup.js
@@ -0,0 +1,72 @@
+/** Показ всплывающего окна при работе с сервером */
+
+(function () {
+
+
+    // Функция для создания и стилизации всплывающего окна:
+    let createPopUp = function () {
+        let popUp = document.createElement('div');
+        popUp.style.position = 'fixed';
+        popUp.style.top = '50%';
+        popUp.style.left = '50%';
+        popUp.style.zIndex = 100;
+        popUp.style.padding = '50px';
+        popUp.style.background = '#fff';
+        popUp.style.border = '5px solid #ff6d51';
+        popUp.style.borderRadius = '20px';
+        popUp.style.transform = 'translate(-50%, -50%)';
+        popUp.style.cursor = 'pointer';
+        popUp.className = 'popup hidden';
+        return popUp;
+    };
+
+
+    // Функция добавления элементов диалогового окна на страницу
+    let renderDialog = function () {
+        let fragment = document.createDocumentFragment();
+        fragment.appendChild(createPopUp());
+        document.body.appendChild(fragment);
+    };
+
+    // Функция показа диалогового окна
+    let showDialog = function (message) {
+        popUp.textContent = message;
+        popUp.classList.remove('hidden');
+    };
+
+    // Функция скрытия диалогового окна
+    let hideDialog = function () {
+        popUp.classList.add('hidden');
+    };
+
+    renderDialog();
+
+    let popUp = document.querySelector('.popup');
+
+
+    popUp.addEventListener("mouseup", function () {
+            hideDialog ();
+        });
+
+
+    window.popup = {
+        // Функция, выводящая окно с ошибкой при неудачной передаче данных
+        errorHandler: function (errorMessage) {
+            showDialog(errorMessage);
+        },
+
+        // Функция, выводящая окно с сообщением об успешной отправке данных и сбрасывающая форму к первоначальному состоянию
+        uploadSuccessHandler: function () {
+            showDialog('Данные успешно отправлены');
+            form.reset();
+        },
+
+        // Функция, выводящая окно с сообщением об успешной загрузке данных с сервера:
+        downloadSuccessHandler: function () {
+            showDialog('Данные успешно загружены');
+        }
+
+    };
+
+
+})();
--- src/js/backend.js
+++ src/js/backend.js
@@ -2,67 +2,62 @@
 
 (function () {
 
-    let URL = "https://js.dump.academy/keksobooking/data";
+    let URL = "https://js.dump.academy/keksobooking";
 
-    let xhr = new XMLHttpRequest();
-    xhr.responseType = 'json';
-    xhr.timeout = 1000;
-    xhr.addEventListener('load', function () {
-        switch (xhr.status) {
-            case 200:
-                onLoad(xhr.response);
-                break;
-            default:
-                onError('Ошибка: ' + xhr.status + ' ' + xhr.statusText);
-        }
-//        console.log (xhr.response);
-    });
-
-    xhr.addEventListener('error', function () {
-        onError('Произошла ошибка соединения');
-    });
-
-    xhr.addEventListener('timeout', function () {
-        onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
-        console.log(onError);
-    });
-
-    xhr.open('GET', URL);
-    xhr.send();
-
-
-    window.upload = function (data, onLoad) {
+    let setup = function (onLoad, onError) {
         let xhr = new XMLHttpRequest();
         xhr.responseType = 'json';
+        xhr.timeout = 10000;
 
         xhr.addEventListener('load', function () {
-            onLoad(xhr.response);
+            switch (xhr.status) {
+                case 200:
+                    onLoad(xhr.response);
+                    break;
+                default:
+                    onError('Ошибка данных: ' + xhr.status + ' ' + xhr.statusText);
+            }
+//        console.log (xhr.response);
         });
-        xhr.open('POST', 'https://js.dump.academy/keksobooking');
+
+        xhr.addEventListener('error', function () {
+            onError('Произошла ошибка соединения');
+        });
+
+        xhr.addEventListener('timeout', function () {
+            onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+            console.log(onError);
+        });
+        return xhr;
+    };
+
+
+    window.upload = function (data, onLoad, onError) {
+        let xhr = setup(onLoad, onError);
+        xhr.open('POST', URL);
         xhr.send(data);
     };
 
 
     window.download = function (onLoad, onError) {
-        let xhr = new XMLHttpRequest();
-        xhr.responseType = 'json';
-        xhr.open('GET', URL);
-
-        xhr.addEventListener('load', function () {
-            onLoad(xhr.response);
-        });
-
+        let xhr = setup(onLoad, onError);
+        xhr.open('GET', URL + '/data');
         xhr.send();
     };
 
 
-    let form = document.querySelector('.notice__form');
-    form.addEventListener('submit', function (evt) {
-        window.upload(new FormData(form), function (response) {
-            form.reset();
-        });
+    window.form = document.querySelector('.notice__form');
+
+    let formSubmit = function (evt) {
+        window.upload(new FormData(form), window.popup.uploadSuccessHandler, window.popup.errorHandler);
+        form.reset();
         evt.preventDefault();
-    });
+    };
+
+    form.addEventListener('submit', formSubmit);
+
+
+    window.download(window.popup.downloadSuccessHandler, window.popup.errorHandler);
 
 
 })();
